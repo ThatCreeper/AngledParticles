1712516712d987cbf67dc4e73d7b890369cc9991
ThatCreeper <creeperhostanimations@gmail.com>
2025-04-09 19:10:30 -0500

    Port to 1.21.1

--- src/main/java/com/rabidsharkgames/angledparticles/mixin/client/EntityRendererMixin.java
+++ src/main/java/com/rabidsharkgames/angledparticles/mixin/client/EntityRendererMixin.java
@@ -5,8 +5,10 @@ import com.rabidsharkgames.angledparticles.Rotator;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityAttachment;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Quaternionf;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,15 +19,16 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
-public class EntityRendererMixin<S extends EntityRenderState>  {
+public class EntityRendererMixin<T extends Entity>  {
 	@Unique
 	private static final Vector3f pos = new Vector3f();
 
 	@Inject(method = "renderNameTag", at = @At(value = "HEAD"))
-	private void renderNameTag(S entityRenderState, Component component, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci) {
-		if (entityRenderState.nameTagAttachment == null) return;
-		pos.set(entityRenderState.nameTagAttachment.x, entityRenderState.nameTagAttachment.y, entityRenderState.nameTagAttachment.z);
-		pos.add((float)entityRenderState.x, (float)entityRenderState.y + 0.5f, (float)entityRenderState.z);
+	private void renderNameTag(T entity, Component component, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, float f, CallbackInfo ci) {
+		Vec3 attachment = entity.getAttachments().getNullable(EntityAttachment.NAME_TAG, 0, entity.getViewYRot(f));
+		if (attachment == null) return;
+		pos.set(attachment.x, attachment.y, attachment.z);
+		pos.add((float)entity.position().x, (float)entity.position().y + 0.5f, (float)entity.position().z);
 	}
 	@Redirect(method = "renderNameTag", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/EntityRenderDispatcher;cameraOrientation()Lorg/joml/Quaternionf;"))
 	private Quaternionf getCameraLookerMat(EntityRenderDispatcher instance) {
